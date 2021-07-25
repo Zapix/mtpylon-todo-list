87c803e51f8189b0403e555c24343c8b1ef49cbf
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-07-25 21:08:07 +0500

    Added auth key table

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import Column, Integer, Sequence, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, Sequence, String, BigInteger, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
 
 
 Base = declarative_base()
@@ -13,5 +13,24 @@ class User(Base):
     nickname = Column(String, unique=True)
     password = Column(String)
 
+    auth_keys = relationship('AuthKey', back_populates='user')
+
     def __repr__(self):
         return f'<User(id={self.id} nickname={self.nickname})>'
+
+
+class AuthKey(Base):
+    """
+    Base class that associates mtpylon auth key with existing `User`
+    Stores auth_key_id as unique_key
+    """
+    __tablename__ = 'auth_keys'
+
+    id = Column(Integer, Sequence('auth_key_id_seq'), primary_key=True)
+    auth_key_id = Column(BigInteger, unique=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship('User', back_populates='auth_keys')
+
+    def __repr__(self):
+        return f'<AuthKey(auth_key_id={self.auth_key_id})>'
