9745f4e54327505ae97f3c696a2b021da21740e6
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-07-26 00:00:51 +0500

    Addeed tasks table

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
 from db import Base
@@ -14,9 +14,24 @@ class TodoList(Base):
     title = Column(String)
 
     user = relationship(User, back_populates='todo_lists')
+    tasks = relationship('Task', back_populates='todo_list')
 
     def __repr__(self):
         return f'<TodoList(id={self.id} title={self.title})>'
 
 
 User.todo_lists = relationship(TodoList, back_populates='user')
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, Sequence('task_id_seq'), primary_key=True)
+    todo_list_id = Column(Integer, ForeignKey('todo_lists.id'))
+    title = Column(String)
+    completed = Column(Boolean, default=False)
+
+    todo_list = relationship('TodoList', back_populates='tasks')
+
+    def __repr__(self):
+        return f'<Task (id={self.id} title={self.title} completed={self.completed})>'
