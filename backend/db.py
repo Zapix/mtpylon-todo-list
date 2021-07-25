c262614304656080ef2e1939a2dd2d198fdd1862
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-07-26 01:40:48 +0500

    Declare sqlalchemy async session

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy.orm import declarative_base
+import os
 
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+PG_URL = os.environ.get('PG_URL')
+
+engine = create_async_engine(PG_URL, future=True, echo=True)
+async_session = sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
 
 Base = declarative_base()
