19060360bf2c83434dec2e5845387cd81e1e45bd
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-07-26 00:30:35 +0500

    Set typings for sqlalchemy models

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from typing import List
 from sqlalchemy import Column, Integer, String, Sequence, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
@@ -9,12 +10,12 @@ from users.models import User
 class TodoList(Base):
     __tablename__ = 'todo_lists'
 
-    id = Column(Integer, Sequence('todo_list_id_seq'), primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    title = Column(String, nullable=False)
+    id: int = Column(Integer, Sequence('todo_list_id_seq'), primary_key=True)
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False)
+    title: str = Column(String, nullable=False)
 
-    user = relationship(User, back_populates='todo_lists')
-    tasks = relationship('Task', back_populates='todo_list')
+    user: User = relationship(User, back_populates='todo_lists')
+    tasks: List['Task'] = relationship('Task', back_populates='todo_list')
 
     def __repr__(self):
         return f'<TodoList(id={self.id} title={self.title})>'
@@ -26,12 +27,16 @@ User.todo_lists = relationship(TodoList, back_populates='user')
 class Task(Base):
     __tablename__ = 'tasks'
 
-    id = Column(Integer, Sequence('task_id_seq'), primary_key=True)
-    todo_list_id = Column(Integer, ForeignKey('todo_lists.id'), nullable=False)
-    title = Column(String, nullable=False)
-    completed = Column(Boolean, default=False, nullable=False)
+    id: int = Column(Integer, Sequence('task_id_seq'), primary_key=True)
+    todo_list_id: int = Column(
+        Integer,
+        ForeignKey('todo_lists.id'),
+        nullable=False
+    )
+    title: str = Column(String, nullable=False)
+    completed: bool = Column(Boolean, default=False, nullable=False)
 
-    todo_list = relationship('TodoList', back_populates='tasks')
+    todo_list: TodoList = relationship('TodoList', back_populates='tasks')
 
     def __repr__(self):
         id_, title, completed = self.id, self.title, self.completed
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
 from sqlalchemy import (
     Column,
     Integer,
@@ -15,11 +17,14 @@ from db import Base
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    nickname = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    id: int = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    nickname: str = Column(String, unique=True, nullable=False)
+    password: str = Column(String, nullable=False)
 
-    auth_keys = relationship('AuthKey', back_populates='user')
+    auth_keys: List['AuthKey'] = relationship(
+        'AuthKey',
+        back_populates='user'
+    )
 
     def __repr__(self):
         return f'<User(id={self.id} nickname={self.nickname})>'
@@ -32,11 +37,11 @@ class AuthKey(Base):
     """
     __tablename__ = 'auth_keys'
 
-    id = Column(Integer, Sequence('auth_key_id_seq'), primary_key=True)
-    auth_key_id = Column(BigInteger, unique=True, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    id: int = Column(Integer, Sequence('auth_key_id_seq'), primary_key=True)
+    auth_key_id: int = Column(BigInteger, unique=True, nullable=False)
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False)
 
-    user = relationship('User', back_populates='auth_keys')
+    user: User = relationship('User', back_populates='auth_keys')
 
     def __repr__(self):
         return f'<AuthKey(auth_key_id={self.auth_key_id})>'
