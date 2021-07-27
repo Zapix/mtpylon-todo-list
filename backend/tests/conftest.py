0a2abd44d574e230f2049f4e1a2479654faaf975
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-07-27 16:48:31 +0500

    Add get user function

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 import pytest
+from faker import Faker
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from db import Base
+from users.models import User
+
+
+@pytest.fixture
+def fake():
+    return Faker()
 
 
 @pytest.fixture
@@ -17,5 +25,17 @@ async def db_engine():
 
 
 @pytest.fixture
-async def async_session(db_engine):
+async def async_session(db_engine) -> sessionmaker:
     return sessionmaker(db_engine, expire_on_commit=False, class_=AsyncSession)
+
+
+@pytest.fixture
+async def user(faker: Faker, async_session: sessionmaker) -> User:
+    async with async_session() as session:
+        user = User(
+            nickname=faker.name(),
+            password='e4ecd6fc11898565af24977e992cea0c9c7b7025'
+        )
+        session.add(user)
+        await session.commit()
+    return user
--- backend/tests/users/test_dal.py
+++ backend/tests/users/test_dal.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from users.dal import create_user
+from users.dal import (
+    create_user,
+    get_user,
+)
 
 
 @pytest.mark.asyncio
 async def test_create_user(async_session):
     async with async_session() as session:
         user = await create_user(session, 'johndoe', 'hashed_password')
+
     assert user.id is not None
 
 
@@ -18,3 +22,39 @@ async def test_create_unique_exception(async_session):
 
         with pytest.raises(ValueError):
             await create_user(session, 'test', 'another_hashed')
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_nickname(async_session, user):
+    async with async_session() as session:
+        returned_user = await get_user(session, nickname=user.nickname)
+
+    assert returned_user is not None
+    assert returned_user.id == user.id
+    assert returned_user.nickname == user.nickname
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id(async_session, user):
+    async with async_session() as session:
+        returned_user = await get_user(session, user_id=user.id)
+
+    assert returned_user is not None
+    assert returned_user.id == user.id
+    assert returned_user.nickname == user.nickname
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_nickname_none(async_session):
+    async with async_session() as session:
+        returned_user = await get_user(session, nickname='fake_user')
+
+    assert returned_user is None
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id_none(async_session):
+    async with async_session() as session:
+        returned_user = await get_user(session, user_id=-1)
+
+    assert returned_user is None
