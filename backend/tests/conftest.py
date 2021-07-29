ae2e3ae1a374b3668d7bc4f10413533f23c69702
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-07-29 15:16:34 +0500

    Set mtpylon as auth_key fixture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,6 +2,7 @@
 import pytest
 from faker import Faker
 
+from mtpylon.crypto import AuthKey  # type: ignore
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
@@ -39,3 +40,9 @@ async def user(faker: Faker, async_session: sessionmaker) -> User:
         session.add(user)
         await session.commit()
     return user
+
+
+@pytest.fixture
+def mtpylon_auth_key() -> AuthKey:
+    auth_key_value = 0x4fa4120d3646e1cadc7e21fcc9c46111ff8467665908a56b18bd38bee60ee1cccc2eff69dda5e638be2b06e813e6d9832142a054d22f405d9d416f79168140d373b048f55924b836ec5be2ab92e29624edf67bd5d763f8a15c3aed587e7ac70f8fe0d78de45c4b9ea5b81a1e0a098eb064dc9609fa37ca33f703b48461aed343aabc1498dd4d1cbbf3ca4c56cc8c7dc315e251e28312ed258c74326729118251f9153f7ac9d52bd47fdf7072963f6330f06bb72e2cc744af91df3302800e80c23c25a402b97bfc5292589b1c688bd1fde0e9997d9996a32ebd39ba258137b123fa762fd07548c44fd1b9321778f6ec3464ae39402c0445bd02fa8223b30cdfc8  # noqa
+    return AuthKey(value=auth_key_value)
