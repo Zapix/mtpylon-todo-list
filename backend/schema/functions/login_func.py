2b21432e5393b3b328c89a55cc16a621a2006714
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-02 14:40:48 +0500

    Remember user after login

--- backend/schema/functions/login_func.py
+++ backend/schema/functions/login_func.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from aiohttp.web import Request
+from mtpylon.crypto import AuthKey  # type: ignore
 from mtpylon.exceptions import RpcCallError  # type: ignore
+from mtpylon.contextvars import auth_key_var  # type: ignore
 
-from users.utils import login_user
+from users.utils import login_user, remember_user
 from ..constructors import User, RegisteredUser
 
 
@@ -11,5 +13,8 @@ async def login(request: Request, nickname: str, password: str) -> User:
         user = await login_user(nickname, password)
     except ValueError as e:
         raise RpcCallError(error_code=401, error_message=str(e))
+
+    auth_key: AuthKey = auth_key_var.get()
+    await remember_user(user, auth_key)
 
     return RegisteredUser(id=user.id, nickname=nickname)
--- backend/tests/schema/functions/test_login.py
+++ backend/tests/schema/functions/test_login.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import pytest
+from contextlib import ExitStack
 from unittest.mock import patch, AsyncMock, MagicMock
 
+from mtpylon.contextvars import auth_key_var  # type: ignore
 from mtpylon.exceptions import RpcCallError  # type: ignore
 
 from schema.functions.login_func import login
@@ -21,15 +23,28 @@ async def test_login_failed():
 
 
 @pytest.mark.asyncio
-async def test_login_success():
+async def test_login_success(mtpylon_auth_key):
+    auth_key_var.set(mtpylon_auth_key)
+
     request = MagicMock()
 
     login_user = AsyncMock()
     login_user.return_value = MagicMock(id=12, nickname='zapix')
 
-    with patch('schema.functions.login_func.login_user', login_user):
+    remember_user = AsyncMock()
+
+    with ExitStack() as patcher:
+        patcher.enter_context(
+            patch('schema.functions.login_func.login_user', login_user)
+        )
+        patcher.enter_context(
+            patch('schema.functions.login_func.remember_user', remember_user)
+        )
+
         user_data = await login(request, 'zapix', 'password123')
 
     assert isinstance(user_data, RegisteredUser)
     assert user_data.id == 12
     assert user_data.nickname == 'zapix'
+
+    remember_user.assert_awaited()
