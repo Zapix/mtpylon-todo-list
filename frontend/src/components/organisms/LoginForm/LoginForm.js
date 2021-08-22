27ade8d3fed14cfed6c7304edf2da00113460d18
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-22 23:56:05 +0500

    Add LoginForm component

--- frontend/src/components/organisms/LoginForm/LoginForm.js
+++ frontend/src/components/organisms/LoginForm/LoginForm.js
@@ -1,39 +1,77 @@
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, Alert } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useFormik } from 'formik';
+
+import validate from './validate';
 
 function LoginForm({ onSubmit: handleSubmit = console.log }) {
+  const formik = useFormik({
+    validate,
+    initialValues: {
+      login: '',
+      password: '',
+    },
+    onSubmit: (values, { setErrors }) => {
+      return handleSubmit(values)
+        .catch((reason) => {
+          setErrors(reason);
+          return Promise.reject(reason);
+        });
+    },
+  });
+
   return (
-    <Form
+    <form
       name="login-form"
+      data-testid="login-form"
+      onSubmit={formik.handleSubmit}
     >
+      <Form.Item>
+        {formik.errors.nonFieldError ? (
+          <Alert
+            data-testid="error-alert"
+            message={formik.errors.nonFieldError}
+            type="error"
+          />
+        ) : null}
+      </Form.Item>
       <Form.Item
         name="login"
+        validateStatus={formik.touched.login && formik.errors.login ? "error" : "success"}
+        help={formik.touched.login && formik.errors.login ? formik.errors.login : null}
       >
         <Input
+          data-testid="login-field"
           size="large"
           placeholder="Login"
           prefix={<UserOutlined/>}
+          {...formik.getFieldProps('login')}
         />
       </Form.Item>
       <Form.Item
-        name="password"
+        validateStatus={formik.touched.password && formik.errors.password ? "error" : "success"}
+        help={formik.touched.password && formik.errors.password ? formik.errors.password: null}
       >
         <Input
+          data-testid="password-field"
           size="large"
           placeholder="Password"
           prefix={<LockOutlined/>}
+          type="password"
+          {...formik.getFieldProps('password')}
         />
       </Form.Item>
       <Form.Item>
         <Button
+          data-testid="submit-button"
           type="primary"
           htmlType="submit"
-          onSubmit={handleSubmit}
+          // disabled={formik.isSubmitting || !formik.isValid}
         >
           Login
         </Button>
       </Form.Item>
-    </Form>
+    </form>
   );
 }
 
