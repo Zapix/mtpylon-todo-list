b7b738d3142d7565150f95e4b9847d25736e46a4
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-24 13:47:51 +0500

    Use LoginFormConrainer for LoginPage

--- frontend/src/components/pages/LoginPage/LoginPage.js
+++ frontend/src/components/pages/LoginPage/LoginPage.js
@@ -1,14 +1,20 @@
-import { Typography } from 'antd';
+import { Typography, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 
-import '../../templates/centered/index.css';
+import { LoginFormContainer } from 'components/organisms/LoginForm';
+import 'components/templates/centered/index.css';
 
 const { Title, Paragraph } = Typography;
 
 function LoginPage() {
   return (
     <div className="centered-page" data-testid="login-page">
-      <Title>Login Page</Title>
+      <Title>Welcome, please login!</Title>
+      <Row>
+        <Col span={12} offset={6}>
+          <LoginFormContainer />
+        </Col>
+      </Row>
       <Paragraph>
         <Link to="/">Back to main</Link>
       </Paragraph>
