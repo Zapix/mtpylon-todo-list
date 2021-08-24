13d3f9edf89840822d97cf4c6eea7b54303d8aac
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-24 14:58:59 +0500

    Add greetings to main page

--- frontend/src/components/pages/MainPage/MainPage.js
+++ frontend/src/components/pages/MainPage/MainPage.js
@@ -1,13 +1,19 @@
 import { Typography } from 'antd';
 import { Link } from 'react-router-dom';
+import { useRecoilValueLoadable } from 'recoil';
+
+import { meAtom } from '../../../state/authentication/atoms';
 
 const { Title, Paragraph } = Typography;
 
 function MainPage() {
+  const me = useRecoilValueLoadable(meAtom);
+
   return (
     <div data-testid="main-page">
       <Title>Main App Page</Title>
       <Paragraph>
+        <Title level={3}>Welcome, {me.contents.nickname}!</Title>
         <Link to="/login">Login</Link>
       </Paragraph>
     </div>
