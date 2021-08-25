79256e861bb31ac9a220a4aa5c89caf3b2a2de0f
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-25 23:21:09 +0500

    Add registration page

--- frontend/src/components/pages/RegistrationPage/RegistrationPage.js
+++ frontend/src/components/pages/RegistrationPage/RegistrationPage.js
@@ -1,25 +1,25 @@
 import { Typography, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
 
-import { LoginFormContainer } from 'components/organisms/LoginForm';
 import 'components/templates/centered/index.css';
 
 const { Title, Paragraph } = Typography;
 
-function LoginPage() {
+function RegistrationPage() {
   return (
-    <div className="centered-page" data-testid="login-page">
-      <Title>Welcome, please login!</Title>
+    <div className="centered-page" data-testid="registration-page">
+      <Title>Welcome, please register!</Title>
       <Row>
         <Col span={12} offset={6}>
-          <LoginFormContainer />
+          Here will be registration form
         </Col>
       </Row>
       <Paragraph>
-        <Link to="/register">Register</Link>
+        Back to <Link to="/">Login</Link>
       </Paragraph>
     </div>
   );
+
 }
 
-export default LoginPage;
+export default RegistrationPage;
