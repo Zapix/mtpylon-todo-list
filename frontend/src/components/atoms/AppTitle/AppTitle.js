bdaf2de0d77d8e8b5b13ef1cbfefd1201ea88e09
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-08-30 20:29:13 +0500

    Add link to main page on logo click

--- frontend/src/components/atoms/AppTitle/AppTitle.js
+++ frontend/src/components/atoms/AppTitle/AppTitle.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import "./AppTitle.css";
+import { Link } from 'react-router-dom';
 
 function AppTitle({ children }) {
-  return <h1 className="AppTitle-title">{children}</h1>
+  return (
+    <h1 className="AppTitle-title">
+      <Link to="/">{children}</Link>
+    </h1>
+  );
 }
 
 export default AppTitle;
