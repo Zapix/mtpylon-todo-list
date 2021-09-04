228b4647287429780aae3aaffda4a488cd60ad92
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-09-04 13:12:22 +0300

    Base AddTaskForm component

---
+++ frontend/src/components/organisms/CreateTaskForm/CreateTaskForm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import { Form, Input, Button } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+import validate from './validate';
+
+function CreateTaskForm({ onSubmit: handleSubmit = () => {} }) {
+  const formik = useFormik({
+    validate,
+    initialValues: {
+      title: '',
+    }
+  });
+
+  return (
+    <Form
+      name="create-task-form"
+      data-testid="create-task-form"
+      layout="inline"
+      onFinish={formik.handleSubmit}
+    >
+      <Form.Item
+        validateStatus={formik.touched.title && formik.errors.title ? 'error' : 'success'}
+        help={formik.touched.title && formik.errors.title ? formik.errors.title : null}
+      >
+        <Input
+          type="text"
+          placeholder="Task..."
+          {...formik.getFieldProps('title')}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button
+          htmlType="submit"
+          type="primary"
+          icon={<PlusOutlined />}
+          disabled={formik.isSubmitting || !formik.isValid}
+        >
+          Add Task
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default CreateTaskForm;
