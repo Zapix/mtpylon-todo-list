8e344634300f252134b517eda1c32fe903f91f9d
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2021-09-09 17:43:40 +0500

    Create task hook

--- frontend/src/state/todolists/hooks.js
+++ frontend/src/state/todolists/hooks.js
@@ -1,7 +1,7 @@
 import { useRecoilStateLoadable } from 'recoil';
 import { methodFromSchema } from 'zagram';
 
-import { todoListsAtomFamily } from './atoms';
+import { todoListsAtomFamily, taskListFamily } from './atoms';
 
 export function useCreateTodoList() {
   const [todoLists, setTodoLists] = useRecoilStateLoadable(todoListsAtomFamily());
@@ -18,5 +18,30 @@ export function useCreateTodoList() {
       .catch(error => {
         return Promise.reject({ nonFieldError: error.errorMessage});
       });
+  };
+}
+
+export function useCreateTask(todoListId) {
+  const [taskList, setTaskList] = useRecoilStateLoadable(taskListFamily(todoListId));
+
+  return (data) => {
+    const rpc = methodFromSchema(
+      window.schema,
+      'create_task',
+      {
+        todo_list_id: todoListId,
+        ...data,
+      }
+    );
+
+    return window.connection.request(rpc).then(data => {
+      console.log('Add new task: ', data);
+      console.log('Existing task list:', taskList);
+      setTaskList([data, ...taskList.contents]);
+      return data;
+    })
+      .catch(error => {
+        return Promise.reject({ nonFieldError: error.errorMessage });
+      })
   };
 }
